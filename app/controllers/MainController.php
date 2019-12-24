caf7139f29e65c78f19880f9041e006e06a4468f
Paiste1 <alexpaiste.ap@gmail.com>
2019-12-24 12:43:13 +0800

    v. 1.2 - сделал таблицы, сделал вывод брендов на главной

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -8,6 +8,8 @@ use \RedBeanPHP\R as R; // тут
 class MainController extends AppController
 {
     public function indexAction() {
+        $brands = R::find('brand',  'LIMIT 3'); // запрос на вывод брендов на главной 3 шт.
         $this->setMeta( 'Главная страница', 'описание...', 'Ключевые фразы...');
+        $this->set(compact('brands'));
     }
 }
--- app/models/AppModel.php
+++ app/models/AppModel.php
@@ -3,7 +3,6 @@
 
 namespace app\models;
 
-
 use ishop\base\Model;
 
 class AppModel extends Model
