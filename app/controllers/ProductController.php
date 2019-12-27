888bd5d9b9191429ac65d2b62be73b139b7a6e77
Paiste1 <alexpaiste.ap@gmail.com>
2019-12-27 14:13:59 +0800

    v. 0.26.2 - сделал галерею товара

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -26,10 +26,11 @@ class ProductController extends AppController
         // просмотренные товары
 
         // галерея
+        $gallery = R::findAll('gallery', 'product_id = ?', [$product->id]);
 
         //модификации товара
 
         $this->setMeta($product['title'], $product['description'], $product['keywords']);
-        $this->set(compact('product', 'related'));
+        $this->set(compact('product', 'related', 'gallery'));
     }
 }
