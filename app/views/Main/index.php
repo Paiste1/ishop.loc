c26bb88501db95eaab7b4b617b6fca181077cb3d
Paiste1 <alexpaiste.ap@gmail.com>
2020-01-08 20:11:33 +0800

    v. 0.27.1 - корзина. получаем данные продукта и модификатора

--- app/views/Main/index.php
+++ app/views/Main/index.php
@@ -60,7 +60,7 @@
                             <h3><a href="product/<?=$hit['alias']?>"><?=$hit['title']?></a></h3>
                             <p>Explore Now</p>
                             <h4>
-                                <a class="add-to-cart-link" href="cart/add?id=<?=$hit['id']?>"><i></i></a> <span class=" item_price"><?=$curr['symbol_left'];?><?=$price;?><?=$curr['symbol_right'];?></span>
+                                <a data-id="<?=$hit['id']?>" class="add-to-cart-link" href="cart/add?id=<?=$hit['id']?>"><i></i></a> <span class=" item_price"><?=$curr['symbol_left'];?><?=$price;?><?=$curr['symbol_right'];?></span>
                                 <?php if($hit['old_price']): ?>
                                     <small><del><?=$curr['symbol_left'];?><?=$price_old;?><?=$curr['symbol_right'];?></del></small>
                                 <?php endif; ?>
