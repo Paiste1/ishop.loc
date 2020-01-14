42a184598649c4f626494caa2e9491a301f38c7a
Paiste1 <alexpaiste.ap@gmail.com>
2020-01-14 20:53:04 +0800

    v. 0.31 - добавил валидатор, делаю регистрацию

---
+++ app/models/User.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace app\models;
+
+class User extends AppModel
+{
+    public $attributes = [ //данные которые идут в регистрацию
+        'login' => '',
+        'password' => '',
+        'name' => '',
+        'email' => '',
+        'address' => '',
+    ];
+
+    public $rules = [
+        'required' => [
+            ['login'],
+            ['password'],
+            ['name'],
+            ['email'],
+            ['address'],
+        ],
+        'email' => [
+            ['email'],
+        ],
+        'lengthMin' => [
+            ['password', 6],
+        ],
+    ];
+}
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -15,6 +15,10 @@ class ComposerStaticInite5b252121c655b47e5d52afc39d1c1c7
         array (
             'app\\' => 4,
         ),
+        'V' => 
+        array (
+            'Valitron\\' => 9,
+        ),
         'R' => 
         array (
             'RedBeanPHP\\' => 11,
@@ -29,6 +33,10 @@ class ComposerStaticInite5b252121c655b47e5d52afc39d1c1c7
         'app\\' => 
         array (
             0 => __DIR__ . '/../..' . '/app',
+        ),
+        'Valitron\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/vlucas/valitron/src/Valitron',
         ),
         'RedBeanPHP\\' => 
         array (
