32347819f20f95f3d1ffbcb943d03459f7de74bf
Paiste1 <alexpaiste.ap@gmail.com>
2020-01-15 20:59:15 +0800

    v. 0.33.3 - реализована отправка писем с заказом админ/клиент

--- config/params.php
+++ config/params.php
@@ -4,6 +4,9 @@ return [
     'admin_email' => 'alexpaiste.ap@gmail.com', // будет приходить информация о заказах
     'shop_name' => 'Магазин Ishop.loc',
     'pagination' => 3,
-    'smtp_login' => '',
-    'smtp_password' => '',
+    'smtp_host' => 'smtp.gmail.com',
+    'smtp_port' => '465',
+    'smtp_protocol' => 'ssl',
+    'smtp_login' => 'alpongn1@gmail.com', //логин почты от которой идет отправка писем
+    'smtp_password' => '132063alex', // пароль см. выше
 ];
