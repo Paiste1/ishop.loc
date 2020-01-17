ecaf77a01c5c906c344de4ac47ee8b7f69729b8e
Paiste1 <alexpaiste.ap@gmail.com>
2020-01-17 12:44:52 +0800

    v. 0.34.3 - фильтр 2 сделан, и включен

--- app/controllers/CategoryController.php
+++ app/controllers/CategoryController.php
@@ -31,14 +31,26 @@ class CategoryController extends AppController
         $sql_part = '';
 
         if(!empty($_GET['filter'])){
-            /*
+            /* фильтр версия 1
              SELECT `product`.`*` FROM `product` WHERE category_id IN (6) AND id IN
                 (
                     SELECT product_id FROM attribute_product WHERE attr_id IN (1,5)
                 );
              */
+            /* фильтр версия 2 (группируем все данные которые выбираются по полю "product_id")
+             SELECT `product`.`*` FROM `product` WHERE category_id IN (6) AND id IN
+                (
+                    SELECT product_id FROM attribute_product WHERE attr_id IN (1,5) GROUP BY product_id HAVING COUNT(product_id) = 2
+                );
+             */
             $filter = Filter::getFilter();
-            $sql_part = "AND id IN (SELECT product_id FROM attribute_product WHERE attr_id IN ($filter))";
+            /*для второго метода*/
+                if($filter){
+                    $cnt = Filter::getCountGroups($filter);
+                    $sql_part = "AND id IN (SELECT product_id FROM attribute_product WHERE attr_id IN ($filter) GROUP BY product_id HAVING COUNT(product_id) = $cnt)";
+                }
+            /*для второго метода*/
+            //$sql_part = "AND id IN (SELECT product_id FROM attribute_product WHERE attr_id IN ($filter))"; // закомментил из за фильтра 2
         }
 
         $total = R::count('product', "category_id IN ($ids) $sql_part");
