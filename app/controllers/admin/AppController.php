5431195b4765cca333783e157c6ed9b418e3be12
Paiste1 <alexpaiste.ap@gmail.com>
2020-01-21 12:23:11 +0800

    v. 0.4.1 - реализовал авторизацию администратора

--- app/controllers/admin/AppController.php
+++ app/controllers/admin/AppController.php
@@ -3,9 +3,20 @@
 
 namespace app\controllers\admin;
 
+use app\models\AppModel;
+use app\models\User;
 use ishop\base\Controller;
 
 class AppController extends Controller
 {
     public  $template = 'admin';
+
+    public function __construct($route)
+    {
+        parent::__construct($route);
+        if(!User::isAdmin() && $route['action'] != 'login-admin'){
+            redirect(ADMIN . '/user/login-admin');
+        }
+        new AppModel();
+    }
 }
