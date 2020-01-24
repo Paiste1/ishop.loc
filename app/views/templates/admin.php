e93a41dab605c60e74417d9519f234fe203bc43b
Paiste1 <alexpaiste.ap@gmail.com>
2020-01-24 13:42:03 +0800

    v. 0.4.6.2 - работа над категориями в админке

--- app/views/templates/admin.php
+++ app/views/templates/admin.php
@@ -585,6 +585,7 @@
 <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 <!-- AdminLTE App -->
 <script src="dist/js/adminlte.min.js"></script>
+<script src="/js/validator.js"></script>
 <script src="my.js"></script>
 
 <?php
