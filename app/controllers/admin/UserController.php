a4e7b1549da8f45fe965a7a3cab8fc0dad679acc
Paiste1 <alexpaiste.ap@gmail.com>
2020-01-29 11:27:50 +0800

    v. 0.4.8 - начал работу над управлением пользователями

--- app/controllers/admin/UserController.php
+++ app/controllers/admin/UserController.php
@@ -3,6 +3,8 @@
 namespace app\controllers\admin;
 
 use app\models\User;
+use ishop\libs\Pagination;
+use RedBeanPHP\R;
 
 class UserController extends AppController
 {
@@ -19,5 +21,23 @@ class UserController extends AppController
             }
         }
         $this->template = 'login';
+    }
+
+    public function editAction(){
+        $user_id = $this->getRequestID();
+        $user = R::load('user', $user_id);
+        $this->setMeta('Редактирование профиля пользователя {$user->name}');
+        $this->set(compact('user'));
+    }
+
+    public function indexAction(){
+        $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+        $perpage = 3;
+        $count = R::count('user');
+        $pagination = new Pagination($page, $perpage, $count);
+        $start = $pagination->getStart();
+        $users = R::findAll('user', "LIMIT $start, $perpage");
+        $this->setMeta('Список пользователей');
+        $this->set(compact('users', 'pagination', 'count'));
     }
 }
