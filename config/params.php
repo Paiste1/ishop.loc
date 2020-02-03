6f0dcb4791afa68260ee8c0a0427c985bae828af
Paiste1 <alexpaiste.ap@gmail.com>
2020-02-03 20:50:59 +0800

    v. 0.5.4 - начал делать загрузку картинок товара главная/галерея

--- config/params.php
+++ config/params.php
@@ -3,10 +3,14 @@
 return [
     'admin_email' => 'alexpaiste.ap@gmail.com', // будет приходить информация о заказах
     'shop_name' => 'Магазин Ishop.loc', // название магазина
-    'pagination' => 3, // сколько товаров будет показано на странице
+    'pagination' => 12, // сколько товаров будет показано на странице
     'smtp_host' => 'smtp.gmail.com',
     'smtp_port' => '465',
     'smtp_protocol' => 'ssl',
     'smtp_login' => 'alpongn1@gmail.com', //логин почты от которой идет отправка писем
     'smtp_password' => '132063alex', // пароль см. выше
+    'img_width' => 125, // ширина главной картинки товара
+    'img_height' => 200, // высота главной картинки товара
+    'gallery_width' => 700, // ширина картинок галереи
+    'gallery_height' => 1000 // высота картинок галереи
 ];
