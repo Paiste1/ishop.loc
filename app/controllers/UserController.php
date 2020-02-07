ea84e53231de79ce5da915c548e3f93d8481e09f
Paiste1 <alexpaiste.ap@gmail.com>
2020-02-07 13:14:26 +0800

    v. 0.5.8 - сделал редактирование данных пользователя в пользовательской части

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -32,19 +32,57 @@ class UserController extends AppController
             $user = new User();
             if($user->login()){
                 $_SESSION['success'] = 'Вы успешно авторизованы!';
+                redirect('/user/cabinet');
             } else {
                 $_SESSION['error'] = 'Логин или пароль введен не верно!';
             }
             redirect();
         }
-        if(@$_SESSION['user']){
+        /*if(@$_SESSION['user']){
             header("refresh: 1.5; url=/");
-        }
+        }*/
         $this->setMeta('Вход');
     }
 
     public function logoutAction(){
         if(isset($_SESSION['user'])) unset($_SESSION['user']);
         redirect();
+    }
+
+    public function cabinetAction(){
+        if(!User::checkAuth()){
+            redirect();
+        }
+        $this->setMeta('Личный кабинет');
+    }
+
+    public function editAction(){
+        if(!User::checkAuth()){
+            redirect('/user/login');
+        }
+        if(!empty($_POST)){
+            $user = new \app\models\admin\User();
+            $data = $_POST;
+            $data['id'] = $_SESSION['user']['id'];
+            $data['role'] = $_SESSION['user']['role'];
+            $user->load($data);
+            if(!$user->attributes['password']){
+                unset($user->attributes['password']);
+            } else {
+                $user->attributes['password'] = password_hash($user->attributes['password'], PASSWORD_DEFAULT);
+            }
+            if(!$user->validate($data) || !$user->checkUnique()){
+                $user->getErrors();
+                redirect();
+            }
+            if($user->update('user', $data['id'])){
+                foreach($user->attributes as $k => $v){
+                    if($k != 'password') $_SESSION['user'][$k] = $v;
+                }
+                $_SESSION['success'] = 'Изменения сохранены!';
+            }
+            redirect();
+        }
+        $this->setMeta('Редактирование данных пользователя');
     }
 }
